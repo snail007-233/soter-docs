35a2877783b1d5e4231ddde1a621359798835ff4
starguoxiaoying@hotmail.com <starguoxiaoying@hotmail.com>
2015-03-31 23:11:03 +0800

    Signed-off-by: starguoxiaoying@hotmail.com <starguoxiaoying@hotmail.com>

--- js/inc.foot.js
+++ js/inc.foot.js
@@ -17,28 +17,30 @@ $(function() {
                 if (lis.length) {
                         $('legend').before('<div><h2 class="title_h2">目录</h2><ol>' + lis.join('') + '</ol></div>');
                 }
+
+                $(".anchor").each(function() {
+                        var link = $(this);
+                        var href = link.attr("href");
+                        if (href && href[0] == "#") {
+                                var name = href.substring(1);
+                                $(this).click(function() {
+                                        var nameElement = $("[name='" + name + "']");
+                                        var idElement = $("#" + name);
+                                        var element = null;
+                                        if (nameElement.length > 0) {
+                                                element = nameElement;
+                                        } else if (idElement.length > 0) {
+                                                element = idElement;
+                                        }
+                                        if (element) {
+                                                var offset = element.offset();
+                                                window.parent.scrollTo(offset.left, offset.top);
+                                        }
+                                        return false;
+                                });
+                        }
+                });
         };
-        
-        $(".anchor").each(function() {
-                var link = $(this);
-                var href = link.attr("href");
-                if (href && href[0] == "#") {
-                        var name = href.substring(1);
-                        $(this).click(function() {
-                                var nameElement = $("[name='" + name + "']");
-                                var idElement = $("#" + name);
-                                var element = null;
-                                if (nameElement.length > 0) {
-                                        element = nameElement;
-                                } else if (idElement.length > 0) {
-                                        element = idElement;
-                                }
-                                if (element) {
-                                        var offset = element.offset();
-                                        window.parent.scrollTo(offset.left, offset.top);
-                                }
-                                return false;
-                        });
-                }
-        });
+
+
 });
