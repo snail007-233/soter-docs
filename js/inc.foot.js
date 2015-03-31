93241c02fd273c02e5067d35408cc473229c2612
starguoxiaoying@hotmail.com <starguoxiaoying@hotmail.com>
2015-03-31 22:41:09 +0800

    修改兼容iframe锚点 Signed-off-by: starguoxiaoying@hotmail.com <starguoxiaoying@hotmail.com>

--- js/inc.foot.js
+++ js/inc.foot.js
@@ -1,21 +1,44 @@
 if (parent == self) {
-	document.write('<p>Soter Powered By:狂奔的蜗牛</p>');
+        document.write('<p>Soter Powered By:狂奔的蜗牛</p>');
 } else {
-	if (document.title) {
-		top.document.title = document.title + "_Soter使用手册";
-	}
+        if (document.title) {
+                top.document.title = document.title + "_Soter使用手册";
+        }
 }
 
-$(function () {
-	if ($('legend')[0]) {
-		var lis = [];
-		$('.title_h2').each(function (index) {
-			var name = 'db_taget_' + index;
-			$(this).before('<a name="' + name + '"></a>');
-			lis.push('<li><a href="#' + name + '">' + $(this).text() + '</a></li>');
-		});
-		if (lis.length) {
-			$('legend').before('<div><h2 class="title_h2">目录</h2><ol>' + lis.join('') + '</ol></div>');
-		}
-	}
+$(function() {
+        if ($('legend')[0]) {
+                var lis = [];
+                $('.title_h2').each(function(index) {
+                        var name = 'db_taget_' + index;
+                        $(this).before('<a name="' + name + '"></a>');
+                        lis.push('<li><a href="#' + name + '">' + $(this).text() + '</a></li>');
+                });
+                if (lis.length) {
+                        $('legend').before('<div><h2 class="title_h2">目录</h2><ol>' + lis.join('') + '</ol></div>');
+                }
+        };
+        
+        $("a").each(function() {
+                var link = $(this);
+                var href = link.attr("href");
+                if (href && href[0] == "#") {
+                        var name = href.substring(1);
+                        $(this).click(function() {
+                                var nameElement = $("[name='" + name + "']");
+                                var idElement = $("#" + name);
+                                var element = null;
+                                if (nameElement.length > 0) {
+                                        element = nameElement;
+                                } else if (idElement.length > 0) {
+                                        element = idElement;
+                                }
+                                if (element) {
+                                        var offset = element.offset();
+                                        window.parent.scrollTo(offset.left, offset.top);
+                                }
+                                return false;
+                        });
+                }
+        });
 });
