eec7ede85e96afe202b3cce87b5057aae43df1d7
snail <arraykeys@gmail.com>
2015-03-31 18:14:25 +0800

    添加js导航
    Signed-off-by: snail <arraykeys@gmail.com>

--- js/inc.foot.js
+++ js/inc.foot.js
@@ -1,7 +1,46 @@
-if(parent==self){
+if (parent == self) {
 	document.write('<p>Soter Powered By:狂奔的蜗牛</p>');
-}else{
-	if(document.title){
-		top.document.title=document.title+"_Soter使用手册";
+} else {
+	if (document.title) {
+		top.document.title = document.title + "_Soter使用手册";
 	}
 }
+$(function () {
+
+	if ($('legend')[0]) {
+		var lis = [];
+		$('.title_h2').each(function (index) {
+			var name = 'db_taget_' + index;
+			$(this).prepend('<a name="' + name + '"></a>');
+			lis.push('<li><a href="#' + name + '">' + $(this).text() + '</a></li>');
+			$(this).append('<a class="gotoTop" style="font-size:10px;margin-left:10px;" href="javascript:void(0);">返回顶部</a>');
+		});
+		if(lis.length){
+			$('legend').before('<div><h2 class="title_h2">目录</h2><ol>' + lis.join('') + '</ol></div>');
+		}
+	}
+});
+$(function(){
+    $('.nav_manual a').on('click',function(){
+        $(this).parent().parent().find('li').removeClass('active');
+        $(this).parent().addClass('active');
+    });
+    var offset = 300;
+    var duration = 500;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('#gotoTop').fadeIn(duration);
+        } else {
+            $('#gotoTop').fadeOut(duration);
+        }
+    });
+    $('#gotoTop,.gotoTop').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        },
+        duration);
+        return false;
+    });
+});
+
