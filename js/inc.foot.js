09bec96456f0103d3b03f8daa6c40fe56ed45155
snail <arraykeys@gmail.com>
2015-04-28 17:48:22 +0800

    Signed-off-by: snail <arraykeys@gmail.com>

--- js/inc.foot.js
+++ js/inc.foot.js
@@ -1,5 +1,5 @@
 if (parent == self) {
-        document.write('<p>Soter Powered By:狂奔的蜗牛</p>');
+        document.write('<p> Powered By Soter &copy; '+new Date().getFullYear()+'</p>');
 } else {
         if (document.title) {
                 top.document.title = document.title + "_Soter使用手册";
